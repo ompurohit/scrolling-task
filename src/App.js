f346bf943252b478d143e3f113e54b0ecd0ebcac
ompurohit <ompurohit353@gmail.com>
2023-02-02 10:44:28 +0530

    task done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from "axios";
+
+export default function App() {
+    
+    const [allData, setAllData] = useState([]);
+    const [filteredData, setFilteredData] = useState(allData);
+    const styles = {
+        display: 'inline',
+        width: '30%',
+        float: 'left',
+        padding: 5,
+        border: '0.5px solid black',
+        marginBottom: 10,
+        marginRight: 10,
+    }
+
+    function removeBackground() {
+        const elements = document.getElementsByClassName('bgYellow');
+        console.log(elements);
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].style.backgroundColor = "transparent";
+        }
+    }
+    
+    const handleSearch = (event) => {
+        let value = +event.target.value;
+        const obj = allData.find(item => item.id === value);
+        const element = obj ? document.getElementById(obj.id) : false;
+        // console.log(element, value);
+        if (element){
+            element.scrollIntoView({
+                beementhavior: "smooth",
+                block: "start",
+                inline: "center",
+            });
+            element.classList.add("bgYellow");
+        }else{
+            removeBackground();
+        }
+    }
+
+
+    useEffect(() => {
+        axios('https://jsonplaceholder.typicode.com/albums/')
+            .then(response => {
+                setAllData(response.data);
+                setFilteredData(response.data);
+            })
+            .catch(error => {
+                console.log('Error getting fake data: ' + error);
+            })
+    }, []);
+    return (
+        <div className="App">
+            <div style={{ margin: '0 auto', marginTop: '5%', marginLeft:'5px' }}>
+                <label>Search:</label>
+                <input type="text" onChange={(event) => handleSearch(event)} placeholder="Enter userId or title"/>
+            </div>
+
+            <div style={{
+                padding: 10,
+                height: '400px',
+                overflowY: 'auto' }}>
+                {filteredData.map((value, index) => {
+                    return (
+                        <div key={value.id}>
+                            <div style={styles} id={value.id} className="transparent">
+                                {value.id} - {value.title} ( {value.userId} )
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+
+    )
+
 }
-
-export default App;
